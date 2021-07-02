f5d5a7c56d04a6d97b337346f1cf64cad33de83c
sonalisharan <angelsona1627@gmail.com>
2021-07-02 16:03:39 +0530

    add local storage support

--- index.js
+++ index.js
@@ -40,6 +40,22 @@ const newCard = ( {
 </div>
 </div>`;
 
+const loadInitialTaskCards = () => {
+    //access localStorage
+    const getInitialData = localStorage.getItem("tasky");
+        if (!getInitialData) return;
+
+    // convert stringified object to object
+    const { cards } = JSON.parse(getInitialData);
+
+    // map around the array to generate HTML card and inject it to DOM
+    cards.map((cardObject) => {
+        const createNewCard = newCard(cardObject);
+        taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+        globalStore.push(cardObject);
+    });
+};
+
 const saveChanges = () => {
     const taskData = {
         id: `${Date.now()}`,  //unique number for card id
@@ -52,5 +68,10 @@ const saveChanges = () => {
     //parent object html -> DOM -> document
 
     const createNewCard = newCard(taskData);
+    
     taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+    globalStore.push(taskData);
+
+    // add to local storage
+    localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
 };
